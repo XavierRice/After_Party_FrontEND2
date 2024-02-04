d77692cf166bec74433efa4e62e4b3e26d1819bb
marciafoster <mfoster9tre@gmail.com>
2024-02-04 14:06:52 -0500

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
-import { Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import AutoPage from "./Pages/AutoPage";
 import Home from "./Pages/Home";
 import Location from "./Pages/Location";
@@ -9,59 +8,45 @@ import Login from "./Pages/Login";
 import Maps from "./Pages/Maps";
 import SignUp from "./Pages/SignUp";
 import GeneralMap from "./Components/GeneralMap";
-import UserError from "./Pages/UserError";
-import CardGenerator from "./Pages/CardGenerator";
-
 import NavBar from "./Components/Navbar";
-
 import PlacesAutocomplete from "./Components/PlacesAutoComplete";
 import Directions from "./Components/Directions";
 import DirectionsMap from "./Pages/DirectionsMap";
 
 function App() {
+  const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
+  const [places, setPlaces] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [LocationString, setLocationString] = useState("");
+  const { lat, lng } = selectedLocation;
+  const backend = 'http://localhost:3001/places';
 
-  const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
-  const [places, setPlaces] = useState([])
-  const [selectedLocation, setSelectedLocation] = useState("")
-  const [LocationString, setLocationString] = useState("")
-  const { lat, lng } = selectedLocation
-  const backend = 'http://localhost:3001/places'
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
-  const [user, setUser] = useState(null)
-  const [token, setToken] = useState(null)
-  
-  const OriginAddress = LocationString.split(" ").slice(0,5).join(" ")
-  console.log(LocationString)
+  const OriginAddress = LocationString.split(" ").slice(0, 5).join(" ");
+  console.log(LocationString);
 
   return (
-
     <div className="App">
       <h1>After Party</h1>
-       <Router> 
+      <Router>
         <NavBar />
-
-        <AutoPage API_KEY={API_KEY} setPlaces={setPlaces} places={places}/>
-        <GeneralMap places={places}/>
-
+        <AutoPage API_KEY={API_KEY} setPlaces={setPlaces} places={places} />
+        <GeneralMap places={places} />
         <main>
-
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/app/Login" element={<Login setUser={setUser} setToken={setToken}/>} />
-            <Route path="/app/SignUp" element={<SignUp setUser={setUser } setToken={setToken}/>} />
+            <Route path="/app/Login" element={<Login setUser={setUser} setToken={setToken} />} />
+            <Route path="/app/SignUp" element={<SignUp setUser={setUser} setToken={setToken} />} />
             <Route path="/app/Location" element={<Location />} />
-            <Route path="/app/Maps" element={<DirectionsMap API_KEY={API_KEY} OriginAddress={OriginAddress}/>} />
+            <Route path="/app/Maps" element={<DirectionsMap API_KEY={API_KEY} OriginAddress={OriginAddress} />} />
             <Route path="*" element={<Error />} />
           </Routes>
-      
-    
+        </main>
+      </Router>
     </div>
   );
-
 }
 
-
-
-
-
-export default App
+export default App;
